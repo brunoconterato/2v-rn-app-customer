6b7f4dcc39ed0d276d2f045a2521b186b3d7de2a
Bruno Conterato <brunoconterato@gmail.com>
2017-12-02 16:57:15 -0200

    App rodando: incluídas chaves de api de Google maps e Facebook, e correção do registro inicial da App.

--- android/app/src/main/java/com/enjoy2v/MainApplication.java
+++ android/app/src/main/java/com/enjoy2v/MainApplication.java
@@ -33,7 +33,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new FBSDKPackage(),
             new MapsPackage(),
               new FBSDKPackage(mCallbackManager)
       );
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 
-AppRegistry.registerComponent('EnjoyV2', () => App);
+AppRegistry.registerComponent('Enjoy2V', () => App);
